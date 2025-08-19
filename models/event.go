5d635c72f52f1ab1670067740e8ad7f4820ecaa9
Rodolfo Díaz <rodolfo.diaz.rovira@gmail.com>
2025-08-19 15:09:47 -0500

    Wrap up Sample REST API.

--- models/event.go
+++ models/event.go
@@ -7,9 +7,12 @@ import (
 )
 
 type Event struct {
-	ID          uuid.UUID
-	Title       string
-	Description string
-	StartTime   time.Time
-	CreatedAt   time.Time
+	ID          uuid.UUID `json:"id"`
+	Title       string    `json:"title" binding:"required,max=100"`
+	Description string    `json:"description,omitempty"`
+	StartTime   time.Time `json:"start_time" binding:"required"`
+	EndTime     time.Time `json:"end_time" binding:"required,gtfield=StartTime"`
+	CreatedAt   time.Time `json:"created_at"`
 }
+
+type Events []Event
